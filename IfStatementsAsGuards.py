b6b394d43e850a63393263b72697a0fd17acbd48
Your Name <taiirbekovag@gmail.com>
2025-06-17 12:49:33 +0600

    If Statements as Guards

--- IfStatementsAsGuards.py
+++ IfStatementsAsGuards.py
@@ -1,11 +1,30 @@
 #You can add if statements in the case evaluation as an extra condition-check:
-month = 5
-day = 4
+#month = 5
+#day = 4
+#match day:
+#    case 1|2|3|4|5 if month ==3:
+#        print("A weekday in March")
+#    case 1|2|3|4|5 if month ==5:
+#        print("A weekday in May")
+#    case _:
+#        print("No match")
+
+#month =11
+#day = 6
+#match day:
+#    case 6|7 if month == 4:
+#        print("Weekends in April ")
+#    case 6|7 if month ==11:
+#        print("Weekends in November")
+#    case _:
+#        print("No match")
+month =11
+day = 6
 match day:
-    case 1|2|3|4|5 if month ==3:
-        print("A weekday in March")
-    case 1|2|3|4|5 if month ==5:
-        print("A weekday in May")
+    case 6|7 if month == 4:
+        print("Weekends in April ")
+    case 6|7 if month ==12:
+        print("Weekends in December")
     case _:
         print("No match")
 
