5dd29c6218a0b124947bbfc04bdea89141fa0fd3
Your Name <taiirbekovag@gmail.com>
2025-06-19 00:38:44 +0600

    Lambda

--- Lambda.py
+++ Lambda.py
@@ -1,5 +1,16 @@
 #x = lambda a: a+ 60
 #print(x(2))
 
-n=  lambda b,c : b * c
-print(n(212,34))
+#n=  lambda b,c : b * c
+#print(n(212,34))
+
+def myfunc(n):
+    return lambda a: a * n
+mydoubler = myfunc(2)
+print(mydoubler(13))
+
+print("----------------------")
+mytripler =myfunc(3)
+print(mytripler(13))
+
+
