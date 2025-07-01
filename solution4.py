0ac5e5868c92f46415d4ce1374874b59945f666f
Your Name <taiirbekovag@gmail.com>
2025-07-01 15:25:10 +0600

    merge 2 dicts

---
+++ solution4.py
@@ -0,0 +1,17 @@
+a = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+}
+b = {
+    "4": 4,
+    "5": 5,
+    "6": 6,
+}
+# z = merge(x, y)
+c = a |b
+print(c)
+# for num in a:
+#     a.append(num)
+#
+# print(a)
