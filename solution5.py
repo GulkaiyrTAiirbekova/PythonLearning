4f9e614746c59a45b7d43d3a6234ff7bb65c04f8
Your Name <taiirbekovag@gmail.com>
2025-07-01 15:36:31 +0600

    top 3 keys

--- solution5.py
+++ solution5.py
@@ -4,6 +4,9 @@
 my_dict = {'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}.
 """
 my_dict = {'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}
-top_keys = sorted(my_dict, key=my_dict.get, reverse=True)[:3]
+# top_keys = sorted(my_dict, key=my_dict.get, reverse=True)[:3]
+#
+# print("Топ 3 ключа:", top_keys)
 
-print("Топ 3 ключа:", top_keys)
+res = sorted(my_dict,key = my_dict.get,reverse =True)[:3]
+print(res)
