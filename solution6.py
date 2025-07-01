554d84a7269c2948dc11b04c23d5bbaf3cc56764
Your Name <taiirbekovag@gmail.com>
2025-07-01 15:48:37 +0600

    solution6

--- solution6.py
+++ solution6.py
@@ -1,12 +1,23 @@
-
-str_a ="90"
+"""str_a ="90"
 b = 23
 c = int(str_a) + b
 print(c)
-
-
-""" #Преобразование string во float """
+"""
+""" #Преобразование string во float 
 str_f = "44.6"
 n = 13.5
 l = float(str_f) + n
 print(l)
+"""
+"""Задача 6
+Напишите код, который переводит целое число в строку, при том что его
+можно применить в любой системе счисления. """
+
+def func(a=0, b=0):
+    if type(a) == int:
+        str_a = str(a)
+        str_b = str(b)
+
+    return str_a + str_b
+
+print(func(5, 5))
