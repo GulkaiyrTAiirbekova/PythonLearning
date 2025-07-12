66b981aedc804ca7465cfa3e3d691e7cd17c4767
Your Name <taiirbekovag@gmail.com>
2025-07-11 16:53:16 -1100

    ceil and floor

---
+++ Math/ceil.py
@@ -0,0 +1,11 @@
+#Import math library
+import math
+
+#Round a number upward to its nearest integer
+x = math.ceil(1.8)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.8)
+
+print(x)
+print(y)
